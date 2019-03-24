d71c6a9825a1552b7e2291e8e9190b5b77ac1025
HystanNASA <kenobi-2002@mail.ru>
2019-03-24 16:24:40 +0300

    rewrite

--- main.c
+++ main.c
@@ -1,96 +1,145 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
-#include <sys/types.h>
 #include <sys/socket.h>
+#include <sys/types.h>
+#include <unistd.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <string.h>
+#include <errno.h>
+#include <signal.h>
 
-const char* HOSTIP;
-int PORT = 17000;
-char FILENAME[256];
+#define PORT 1710
 
-void sendall(const int, char*, int, const int);
-int clientRoutine();
+int     sockfd, mainSock, listenerSock;
+int     file;
+char    filename[256];
 
-int main(int argc, char const *argv[])
+void clientRoutine();
+void serverRoutine();
+void handleSignal();
+
+int main(int argc, char* argv[])
 {
-    int retValue;
-
-    if(argc < 3)
+    if(argc != 3)
     {
-        perror("too few args");
-        return 1;
+        fprintf(stderr, "Must be 2 args\n");
+        exit(1);
     }
 
-    HOSTIP = argv[1];
-    strcpy(FILENAME, argv[2]);  
+    signal(SIGABRT, handleSignal);
 
-    retValue = clientRoutine();
+    strcpy(filename, argv[2]);
+    if(strcmp(argv[1], "-s") == 0)
+    {
+        serverRoutine();
+    }
+    else if(strcmp(argv[1], "-c") == 0) 
+    {
+        clientRoutine();
+    }
+    else
+    {
+        fprintf(stderr, "Must be the -s or -c argument\n");
+    }
 
     return 0;
 }
 
-int clientRoutine()
+void clientRoutine()
 {
-    int                 sockfd;
     struct sockaddr_in  addr;
-    FILE*               file;
-    char                buffer[1024];
-    int                 localerrno = 0;
+    char                data[1024];
 
     addr.sin_family      = AF_INET;
     addr.sin_port        = htons(PORT);
-    addr.sin_addr.s_addr = inet_addr(HOSTIP);
+    addr.sin_addr.s_addr = inet_addr("192.168.0.12");
+    memset(&(addr.sin_zero), 0, sizeof(addr.sin_zero));
 
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
     if(sockfd < 0)
     {
-        perror("socket error.");
-        localerrno = 1;
+        fprintf(stderr, "Socket failed\n");
         goto terminate;
     }
 
     if(connect(sockfd, (struct sockaddr*)&addr, sizeof(addr)) < 0)
     {
-        perror("connect error.");
-        localerrno = 1;
+        fprintf(stderr, "Connection failed\n");
         goto terminate;
     }
 
-    printf("Connected\n");
+    int totalBytesWritten = 0;
 
-    char listeningBuffer[10];
-    sendall(sockfd, FILENAME, strlen(FILENAME), 0);
-    printf("Sent filename\n");
-    recv(sockfd, listeningBuffer, 10, 0);
-
-    file = fopen(FILENAME, "rb");
-    if(file == NULL)
+    if(file = fopen(filename, "rb") < 0)
+    {
+        fprintf(stderr, "Opening file failed\n");
         goto terminate;
-
-    char c = 1;
-    while(c != EOF)
-    {   
-        for(int i = 0; ((c = fgetc(file)) != EOF) && (i < 1024); i++)
-            buffer[i] = c;
-        sendall(sockfd, buffer, strlen(buffer), 0);
     }
-    fclose(file);
+
+    /* Communicating with server */
+    write(sockfd, "ok", sizeof("ok"));
 
 terminate:
     close(sockfd);
-    return localerrno;
+    close(file);
 }
 
-void sendall(const int sockfd, char* buf, int len, const int flags)
+void serverRoutine()
 {
-    while(1)
+    struct sockaddr_in  addr;
+    char                filename[256];
+    char                data[1024];
+    const int           trueFlag = 1;
+
+    addr.sin_family      = AF_INET;
+    addr.sin_port        = htons(PORT);
+    addr.sin_addr.s_addr = INADDR_ANY;
+    memset(&(addr.sin_zero), 0, sizeof(addr.sin_zero));
+
+    mainSock = socket(AF_INET, SOCK_STREAM, 0);
+    if(mainSock < 0)
     {
-        int i = send(sockfd, buf + i, len, flags);
-        if(i < 1)   break;
-        len -= i;
-        buf += i;
+        fprintf(stderr, "Scoket failed\n");
+        goto terminate;
     }
+    
+    if(setsockopt(mainSock, SOL_SOCKET, SO_REUSEADDR, &trueFlag, sizeof(int)) < 0)
+    {
+        fprintf(stderr, "setsockopt failed\nErrno: %d", errno);
+        goto terminate;
+    }
+
+    if(bind(mainSock, (struct sockaddr*)&addr, sizeof(addr)) < 0)
+    {
+        fprintf(stderr, "Bindning failed\nErrno: %d\n", errno);
+        goto terminate;
+    }
+
+    if(listen(mainSock, 5) < 0)
+    {
+        fprintf(stderr, "Listening failed\n");
+        goto terminate;
+    }
+
+    listenerSock = accept(mainSock, NULL, NULL);
+
+    /* Communicating with client */
+    char buff[16];
+    read(listenerSock, buff, sizeof(buff));
+
+    printf("%s\n", buff);
+
+terminate:
+    close(mainSock);
+    close(listenerSock);
+    close(file);
+}
+
+void handleSignal()
+{
+    close(sockfd);
+    close(mainSock);
+    close(listenerSock);
+    close(file);
 }
